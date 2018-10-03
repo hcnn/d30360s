a5c71f228616ca5682b4c154c3bb30e3533fe414
UH <554c46@gmail.com>
2018-10-03 22:28:12 +0200

    initial upload

---
+++ d30360s.h
@@ -0,0 +1,10 @@
+#ifndef D30360S_H
+#define D30360S_H
+
+#include <stdlib.h> //exit
+#include <stdio.h>
+
+double d30360s(int y1, int m1, int d1, 
+               int y2, int m2, int d2);
+
+#endif /* D30360S_H */
